706e2f5c6042f6354a2328b479a64d94aa9431b8
abhigyan-709 <abhigkumar709@gmail.com>
2025-01-09 16:45:53 +0530

    setup for ec2 instance cluster

--- infrastructure/ec2-k8-master.tf
+++ infrastructure/ec2-k8-master.tf
@@ -14,13 +14,107 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical's AWS Account ID
 }
 
+# Define IAM role for Kubernetes (with Admin access)
+resource "aws_iam_role" "k8s_role" {
+  name = "k8s-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
+# Attach Admin policy to Kubernetes IAM role
+resource "aws_iam_role_policy_attachment" "k8s_admin_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.k8s_role.name
+}
+
+# Define IAM instance profile for Kubernetes
+resource "aws_iam_instance_profile" "k8s_instance_profile" {
+  name = "k8s-instance-profile"
+  role = aws_iam_role.k8s_role.name
+}
+
+# Define Security Group for Kubernetes EC2 instance
+resource "aws_security_group" "k8s_security_group" {
+  name        = "k8s_security_group"
+  description = "Allow Kubernetes access"
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2379
+    to_port     = 2380
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 10259
+    to_port     = 10259
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 10257
+    to_port     = 10257
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # For Flannel VXLAN network communication
+  ingress {
+    from_port   = 8472
+    to_port     = 8472
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # NodePort services
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Egress rules (allow all outbound traffic)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance for Kubernetes master node
 resource "aws_instance" "ec2-master-cluster" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.medium"
   key_name               = aws_key_pair.mongo_key.key_name
-  security_groups        = [aws_security_group.mongo_security_group.name]
-  iam_instance_profile   = aws_iam_instance_profile.admin_profile.name
+  security_groups        = [aws_security_group.k8s_security_group.name]
+  iam_instance_profile   = aws_iam_instance_profile.k8s_instance_profile.name
 
   user_data = <<-EOF
     #!/bin/bash
--- infrastructure/security-group.tf
+++ infrastructure/security-group.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "mongo_security_group" {
   name        = "mongo_security_group"
-  description = "Allow MongoDB, Kubernetes, and SSH access"
+  description = "Allow MongoDB and SSH access"
 
   ingress {
     from_port   = 22
@@ -16,59 +16,6 @@ resource "aws_security_group" "mongo_security_group" {
     cidr_blocks = ["0.0.0.0/0"]  # Restrict this in production
   }
 
-  # Kubernetes related ports
-  ingress {
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 2379
-    to_port     = 2380
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 10250
-    to_port     = 10250
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 10259
-    to_port     = 10259
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 10257
-    to_port     = 10257
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # For Flannel VXLAN network communication
-  ingress {
-    from_port   = 8472
-    to_port     = 8472
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # NodePort services
-  ingress {
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Egress rules (allow all outbound traffic)
   egress {
     from_port   = 0
     to_port     = 0
