488418487a06c326b542230b46b1c21b115519e7
abhigyan-709 <abhigkumar709@gmail.com>
2025-01-09 16:10:41 +0530

    changes in ec2 cluster

--- infrastructure/ec2-k8-master.tf
+++ infrastructure/ec2-k8-master.tf
@@ -1,20 +1,3 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical's AWS Account ID
-}
-
-
 resource "aws_instance" "ec2-master-cluster" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.medium"
@@ -23,22 +6,31 @@ resource "aws_instance" "ec2-master-cluster" {
   iam_instance_profile   = aws_iam_instance_profile.admin_profile.name
 
   user_data = <<-EOF
-      #!/bin/bash
-      set -e
-      sudo apt update
-      sudo apt install -y apt-transport-https ca-certificates curl
-      curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-      cat <<EOF2 | sudo tee /etc/apt/sources.list.d/kubernetes.list
-      deb https://apt.kubernetes.io/ kubernetes-xenial main
-      EOF2
-      sudo apt update
-      sudo apt install -y kubelet kubeadm kubectl
-      sudo apt-mark hold kubelet kubeadm kubectl
-      sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-      mkdir -p $HOME/.kube
-      sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-      sudo chown $(id -u):$(id -g) $HOME/.kube/config
-      kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+    #!/bin/bash
+    set -e
+    # Update and install dependencies
+    sudo apt update
+    sudo apt install -y apt-transport-https ca-certificates curl
+
+    # Add Kubernetes repository and install Kubernetes components
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+    sudo tee /etc/apt/sources.list.d/kubernetes.list <<EOF2
+    deb https://apt.kubernetes.io/ kubernetes-xenial main
+    EOF2
+    sudo apt update
+    sudo apt install -y kubelet kubeadm kubectl
+    sudo apt-mark hold kubelet kubeadm kubectl
+
+    # Initialize the Kubernetes master node
+    sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+    # Set up kubeconfig for the ubuntu user
+    mkdir -p $HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+    # Install Flannel CNI plugin
+    kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
   EOF
 
   tags = {
