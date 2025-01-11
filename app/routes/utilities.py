15de665c78d34a44259ffc59e79a2df6eaf111b9
abhigyan-709 <abhigkumar709@gmail.com>
2025-01-12 04:35:54 +0530

    update the routes

--- app/routes/utilities.py
+++ app/routes/utilities.py
@@ -18,3 +18,23 @@ async def add_utilites(utility: PythonUtilities, current_user: User = Depends(ge
         return JSONResponse(content={"message": "Utility added successfully"}, status_code=201)
     except Exception as e:
         return JSONResponse(content={"message": str(e)}, status_code=500)
+    
+@route3.get("/get_utilities", tags=["utilities"])
+async def get_utilities(current_user: User = Depends(get_current_user), db_client: MongoClient = Depends(db.get_client)):
+    if current_user.role != "admin":
+        return JSONResponse(content={"message": "You are not authorized to perform this action"}, status_code=401)
+    try:
+        utilities = db_client[db.db_name]["utilities"].find()
+        return JSONResponse(content={"message": "Utilities fetched successfully", "data": list(utilities)}, status_code=200)
+    except:
+        return JSONResponse(content={"message": "Error fetching utilities"}, status_code=500)
+    
+@route3.get("/get_utilities_by_category/{category}", tags=["utilities"])
+async def get_utilities_by_category(category: str, current_user: User = Depends(get_current_user), db_client: MongoClient = Depends(db.get_client)):
+    if current_user.role != "admin":
+        return JSONResponse(content={"message": "You are not authorized to perform this action"}, status_code=401)
+    try:
+        utilities = db_client[db.db_name]["utilities"].find({"category": category})
+        return JSONResponse(content={"message": "Utilities fetched successfully", "data": list(utilities)}, status_code=200)
+    except:
+        return JSONResponse(content={"message": "Error fetching utilities"}, status_code=500)
